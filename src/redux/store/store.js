286a3fc1e74ddd9a484f90001ba839723089e137
zeeshan <javeedzeeshan03@gmail.com>
2022-08-03 18:39:51 +0500

    added

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,80 +1,41 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import Color_theme from "../reducer/Color_theme";
-// import { combineReducers } from "redux";
-// import { persistReducer, persistStore } from "redux-persist";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// const persistConfig = {
-//    key: "root",
-//    storage: AsyncStorage,
-// };
-
-// const rootReducer = combineReducers({
-//    Color_theme: Color_theme,
-// });
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//    reducer: persistedReducer,
-
-//    middleware: (getDefaultMiddleware) =>
-//       getDefaultMiddleware({
-//          immutableCheck: false,
-//          serializableCheck: false,
-//       }),
-// });
-// export const persistor = persistStore(store);
-
-// import {configureStore} from '@reduxjs/toolkit';
-// import counterReducer from './counterSlice';
-
-// export default configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//   },
-// });
-
-// REDUX-PERSIST TRIAL
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import Color_theme from "../reducer/Color_theme";
 import chat from "../reducer/chat";
 import allUsers from "../reducer/allUsers";
 import { combineReducers } from "redux";
 import {
-   FLUSH,
-   PAUSE,
-   PERSIST,
-   persistReducer,
-   persistStore,
-   PURGE,
-   REGISTER,
-   REHYDRATE,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const rootReducer = combineReducers({
-   ColorTheme: Color_theme,
-   chat: chat,
-   allUsers: allUsers,
+  ColorTheme: Color_theme,
+  chat: chat,
+  allUsers: allUsers,
 });
 
 const persistConfig = {
-   key: "root",
-   storage: AsyncStorage,
+  key: "root",
+  storage: AsyncStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-   reducer: persistedReducer,
+  reducer: persistedReducer,
 
-   // middleware option needs to be provided for avoiding the error. ref: https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
-   middleware: getDefaultMiddleware({
-      serializableCheck: {
-         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-   }),
+  // middleware option needs to be provided for avoiding the error. ref: https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export const persistor = persistStore(store);
--- src/theme/change_theme.js
+++ src/theme/change_theme.js
@@ -1,8 +1,5 @@
 import { THEME } from "./index";
 
 export const ColorTheme = (e) => {
-  return {
-    //  color: e ? THEME.COLORS : THEME.LightTHEME,
-    color: e ? THEME.LightTHEME : THEME.COLORS,
-  };
+  return {};
 };
